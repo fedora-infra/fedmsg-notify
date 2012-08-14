2ba111b8cc7ebc77581c08c7396d958a42edaf39
Ross Delinger <rossdylan@csh.rit.edu>
2012-08-13 20:29:51 -0400

    Switched over to directly using the consumer to display notifications instead of going through another client application

--- fedmsg_notify_consumer/notify_consumer.py
+++ fedmsg_notify_consumer/notify_consumer.py
@@ -1,8 +1,9 @@
 from paste.deploy.converters import asbool
 from fedmsg.consumers import FedmsgConsumer
+import fedmsg.text
 import logging
-import zmq
-import json
+import pynotify
+
 
 log = logging.getLogger("moksha.hub")
 
@@ -15,14 +16,9 @@ class NotifyConsumer(FedmsgConsumer):
         if not asbool(hub.config.get(ENABLED, False)):
             log.info(
                 'fedmsg.consumers.notifyconsumer disbaled')
-        self.context = zmq.Context()
-        self.subscribers = self.context.socket(zmq.PUB)
-        settings = self.hub.config.get("notify")
-
-        listen_uri = settings.get(
-                'listener_uri',
-                'tcp://127.0.0.1:9934')
-        self.subscribers.bind(listen_uri)
+        pynotify.init("Fedmsg")
 
     def consume(self, msg):
-        self.subscribers.send(json.dumps(msg))
+        pretty_text = fedmsg.text.msg2repr(msg)
+        note = pynotify.Notification(pretty_text)
+        note.show()
