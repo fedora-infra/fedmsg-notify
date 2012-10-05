1b4d79459b6cab5a39fca4f374190eb93f1ed2a7
Luke Macken <lmacken@redhat.com>
2012-10-05 00:21:48 -0400

    Create a main method in our gui module

--- fedmsg_notify/gui.py
+++ fedmsg_notify/gui.py
@@ -72,6 +72,9 @@ class FedmsgNotifyConfigApp(Gtk.Application):
         Gtk.Application.do_startup(self)
 
 
-if __name__ == '__main__':
+def main():
     app = FedmsgNotifyConfigApp()
     sys.exit(app.run(sys.argv))
+
+if __name__ == '__main__':
+    main()
