8586b61b60a7ba20383ef66aeb36924ba118df1b
Luke Macken <lmacken@redhat.com>
2012-10-19 14:48:30 -0400

    Add on/off switches for all fedmsg text processors

--- fedmsg_notify/gui.py
+++ fedmsg_notify/gui.py
@@ -18,6 +18,7 @@
 
 import sys
 import dbus
+import fedmsg.text
 
 from gi.repository import Gtk, Gio
 
@@ -25,6 +26,8 @@ from gi.repository import Gtk, Gio
 class FedmsgNotifyConfigWindow(Gtk.ApplicationWindow):
     bus_name = 'org.fedoraproject.fedmsg.notify'
     obj_path = '/org/fedoraproject/fedmsg/notify'
+
+    _switches = {}  # {Gtk.Switch: signal connection}
 
     def __init__(self, app):
         Gtk.Window.__init__(self, title="fedmsg-notify-config", application=app)
@@ -35,8 +38,18 @@ class FedmsgNotifyConfigWindow(Gtk.ApplicationWindow):
         self.bus = dbus.SessionBus()
         running = self.bus.name_has_owner(self.bus_name)
 
-        self.switch = Gtk.Switch()
+        self.all_switch = Gtk.Switch()
+        self.all_toggle_label = Gtk.Label()
+        self.all_toggle_label.set_text("Fedmsg Desktop Notifications")
+        self.grid = Gtk.Grid()
+        self.grid.set_column_spacing(10)
+        self.grid.attach(self.all_toggle_label, 1, 0, 1, 1)
+        self.grid.attach(self.all_switch, 0, 0, 1, 1)
+        self.add(self.grid)
+        self.scan_text_processors()
+
         self.connect_signal_handlers()
+
         enabled = self.settings.get_boolean('enabled')
         if enabled and not running:
             self.toggle_service(True)
@@ -44,17 +57,29 @@ class FedmsgNotifyConfigWindow(Gtk.ApplicationWindow):
             self.toggle_service(False)
         elif enabled and running:
             self.disconnect_signal_handlers()
-            self.switch.set_active(True)
+            self.all_switch.set_active(True)
             self.connect_signal_handlers()
 
-        label = Gtk.Label()
-        label.set_text("Fedmsg Desktop Notifications")
+    def scan_text_processors(self):
+        """ Create an on/off switch for each fedmsg text processor """
+        top_label = self.all_toggle_label
+        top_switch = self.all_switch
+        for i, processor in enumerate(fedmsg.text.processors):
+            label = Gtk.Label()
+            label.set_text(processor.__obj__)
+            switch = Gtk.Switch()
+            switch.__name__ = processor.__obj__
+            self._switches[switch] = None
+            self.grid.attach_next_to(label, top_label,
+                                     Gtk.PositionType.BOTTOM, 1, 1)
+            self.grid.attach_next_to(switch, top_switch,
+                                     Gtk.PositionType.BOTTOM, 1, 1)
+            top_label = label
+            top_switch = switch
 
-        grid = Gtk.Grid()
-        grid.set_column_spacing(10)
-        grid.attach(label, 1, 0, 1, 1)
-        grid.attach(self.switch, 0, 0, 1, 1)
-        self.add(grid)
+    def activate_filter_switch(self, button, active):
+        """ Called when the text processor specific filters are selected """
+        print(button.__name__)
 
     def activate_cb(self, button, active):
         self.toggle_service(button.get_active())
@@ -64,14 +89,14 @@ class FedmsgNotifyConfigWindow(Gtk.ApplicationWindow):
             notify_obj = self.bus.get_object(self.bus_name, self.obj_path)
         except dbus.exceptions.DBusException, e:
             print(str(e))
-            self.switch.set_active(False)
+            self.all_switch.set_active(False)
             return False
         notify_iface = dbus.Interface(notify_obj, self.bus_name)
         if state:
             notify_iface.Enable()
         else:
             notify_iface.Disable()
-        self.switch.set_active(state)
+        self.all_switch.set_active(state)
         self.disconnect_signal_handlers()
         self.settings.set_boolean('enabled', state)
         self.connect_signal_handlers()
@@ -79,16 +104,23 @@ class FedmsgNotifyConfigWindow(Gtk.ApplicationWindow):
     def connect_signal_handlers(self):
         self.setting_conn = self.settings.connect(
             'changed::enabled', self.settings_changed)
-        self.switch_conn = self.switch.connect(
+        self.switch_conn = self.all_switch.connect(
             "notify::active", self.activate_cb)
+        for switch in self._switches:
+            self._switches[switch] = switch.connect(
+                'notify::active', self.activate_filter_switch)
 
     def disconnect_signal_handlers(self):
         self.settings.disconnect(self.setting_conn)
-        self.switch.disconnect(self.switch_conn)
+        self.all_switch.disconnect(self.switch_conn)
+        for switch, switch_conn in self._switches.iteritems():
+            print(repr(switch_conn))
+            switch.disconnect(switch_conn)
+            self._switches[switch] = None
 
     def settings_changed(self, settings, key):
         self.disconnect_signal_handlers()
-        self.switch.set_active(settings.get_boolean(key))
+        self.all_switch.set_active(settings.get_boolean(key))
         self.connect_signal_handlers()
 
 
