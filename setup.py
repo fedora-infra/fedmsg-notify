84eae707ff5a41a569117c84769c6c29bf01d5c2
Luke Macken <lmacken@redhat.com>
2013-06-18 13:18:30 -0400

    0.5.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ requires = [
 
 setup(
         name="fedmsg-notify",
-        version='0.5.0',
+        version='0.5.1',
         description="Consumer for fedmsg that spits out libnotify desktop notifications",
         author="Luke Macken, Ross Delinger",
         author_email="lmacken@redhat.com",
