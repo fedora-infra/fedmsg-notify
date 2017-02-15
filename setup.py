5de8707b3b6506977ca8afe368e0825299ca9bab
Aurélien Bompard <aurelien@bompard.org>
2017-02-15 14:53:50 +0100

    Version 0.5.7

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ requires = [
 
 setup(
         name="fedmsg-notify",
-        version='0.5.6',
+        version='0.5.7',
         description="Consumer for fedmsg that spits out libnotify desktop notifications",
         author="Luke Macken, Ross Delinger",
         author_email="lmacken@redhat.com",
